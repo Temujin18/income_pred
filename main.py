af6117c96dbd2e5f9ef28b700615143320a1730a
John Exim <jonh.exim.ece@gmail.com>
2020-10-11 04:27:08 +0800

    Fix whitespacing

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from typing import List
-
 from fastapi import FastAPI
 from joblib import load
-import pandas as pd
-import redis
 from datetime import timedelta
-
 from base_models import Features
 from training.training import le, scaler, X, categorical
-
+import pandas as pd
+import redis
 
 app = FastAPI()
-
 model = load("model/census_model.joblib")
-
 r = redis.Redis()
 
+
 def encode_input(X_input):
+    
     for category in categorical:
         X_input[category] = le.fit_transform(X_input[category])
     
     X_input = pd.DataFrame(scaler.fit_transform(X_input), index=X_input['user_id'], columns=X_input.columns)
 
     return X_input.drop('user_id', axis=1)
+
 
 @app.post("/predict/")
 async def predict_income(features: List[Features]):
@@ -33,7 +31,6 @@ async def predict_income(features: List[Features]):
         X_input = X_input.append(feature.__values__, ignore_index=True)
 
     users = X_input['user_id'].astype(int)
-
     X_input = encode_input(X_input)
 
     with r.pipeline() as pipe:
@@ -44,8 +41,6 @@ async def predict_income(features: List[Features]):
                 pred = model.predict(X_input.loc[user].values.reshape(1,-1))
                 pipe.setex(user, timedelta(minutes=10), ''.join(pred))
                 result.append({'user_id' : user, 'income' : ''.join(pred)})
-
         pipe.execute()
     
-
     return result
